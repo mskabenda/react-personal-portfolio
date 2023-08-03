c180fe7d8cfbf29a7e9c6ffb52c77f43c3f34825
Mitchell K <128099400+mskabenda@users.noreply.github.com>
2023-08-03 14:57:21 -0500

    Update ProjectList.js
    Added image of Simple Calculator

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,6 @@
 import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.jpg"
-
+import Proj3 from "../assets/Proj3.png"
 export const ProjectList = [
   {
     name: "Personal Portfolio",
@@ -13,4 +13,12 @@ export const ProjectList = [
     image: Proj2,
     skills: "JavaScript,HTML,CSS, React, JSX,",
   },
+
+  {
+    name: "Simple Calculator",
+    image: Proj3,
+    skills: "HTML,CSS, React,",
+  },
+
+
 ];
